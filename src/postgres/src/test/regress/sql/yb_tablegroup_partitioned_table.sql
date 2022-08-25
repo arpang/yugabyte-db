eea1e99c1ccc424e8853e27d9962647e1f693e9a
Arpan Agrawal <arpang@users.noreply.github.com>
2022-08-25 13:58:34 +0530

    Test fix

--- src/postgres/src/test/regress/sql/yb_tablegroup_partitioned_table.sql
+++ src/postgres/src/test/regress/sql/yb_tablegroup_partitioned_table.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE test_tablegroup_partitioned_tables;
+\c test_tablegroup_partitioned_tables
 CREATE TABLEGROUP tg1;
 CREATE TABLE prt (id int PRIMARY KEY, v int) PARTITION BY RANGE (id) TABLEGROUP tg1;
 CREATE TABLE prt_p1 PARTITION OF prt FOR VALUES FROM (1) TO (4);
