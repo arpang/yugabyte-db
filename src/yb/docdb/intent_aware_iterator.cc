9d262bff64c0465d7849d90651fa5de1ce1de655
Arpan Agrawal <arpan3495@gmail.com>
2022-12-15 16:23:40 +0530

    Minor todo

--- src/yb/docdb/intent_aware_iterator.cc
+++ src/yb/docdb/intent_aware_iterator.cc
@@ -491,6 +491,7 @@ Result<FetchKeyResult> IntentAwareIterator::FetchKey() {
     result.write_time = GetIntentDocHybridTime();
     result.same_transaction = ResolvedIntentFromSameTransaction();
     bool is_metadata_key = VERIFY_RESULT(IsMetadataKey(result.key));
+    // TODO: 
     if (!is_metadata_key || metadata_iterator_) {
       max_seen_ht_.MakeAtLeast(resolved_intent_txn_dht_.hybrid_time());
     }
