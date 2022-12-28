1a2328d407f1549c4d3f8b8c627d975888dad006
Arpan Agrawal <arpan3495@gmail.com>
2022-12-28 21:42:12 +0530

    Updates todos

--- src/yb/docdb/value_type.h
+++ src/yb/docdb/value_type.h
@@ -118,7 +118,8 @@ namespace docdb {
     ((kMergeFlags, 'k')) /* ASCII code 107 */ \
     ((kBitSet, 'm')) /* ASCII code 109 */ \
     ((kSubTransactionId, 'n')) /* ASCII code 110 */ \
-    ((kTabletMetadata, 'o')) /* ASCII code 111 */ /* TODO: is the prefix ok? */ \
+    /* Prefix for metadata entries */ \
+    ((kTabletMetadata, 'o')) /* ASCII code 111 */ \
     /* Timestamp value in microseconds */ \
     ((kTimestamp, 's'))  /* ASCII code 115 */ \
     /* TTL value in milliseconds, optionally present at the start of a value. */ \
