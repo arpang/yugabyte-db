c6e06ee1f2f8ae2135525351a3c87d62fc408fda
Arpan Agrawal <arpan3495@gmail.com>
2023-03-30 08:51:21 +0530

    Adds a todo

--- src/yb/tablet/tablet_peer.cc
+++ src/yb/tablet/tablet_peer.cc
@@ -1004,8 +1004,8 @@ Result<int64_t> TabletPeer::GetEarliestNeededLogIndex(std::string* details) cons
     }
   }
 
+  // TODO: Only when lazy flush is on
   auto min_unflushed_change_metadata_index = meta_->MinUnflushedChangeMetadataOpId().index;
-
   min_index = std::min(min_index, min_unflushed_change_metadata_index);
   if (details) {
     *details +=
