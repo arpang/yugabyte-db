db559268edb14d12ffe396a7dee4abf9405a033b
Arpan Agrawal <arpan3495@gmail.com>
2023-07-10 18:24:32 +0530

    Initial commit: catalog directory fixes

--- src/postgres/src/backend/catalog/yb_catalog/yb_type.c
+++ src/postgres/src/backend/catalog/yb_catalog/yb_type.c
@@ -105,10 +105,6 @@ YbDataTypeFromOidMod(int attnum, Oid type_id)
 			case SelfItemPointerAttributeNumber: /* ctid */
 				type_id = TIDOID;
 				break;
-			#ifdef YB_TODO
-			/* OID is a regular column PG15 onwards. */
-			case ObjectIdAttributeNumber: /* oid */
-			#endif
 			case TableOidAttributeNumber: /* tableoid */
 				type_id = OIDOID;
 				break;
