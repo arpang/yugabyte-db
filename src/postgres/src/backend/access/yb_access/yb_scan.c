d2d2c78429b1f9bf3588e659b1a8cd27125fd966
Arpan Agrawal <arpan3495@gmail.com>
2023-10-05 21:14:22 +0530

    Minor change

--- src/postgres/src/backend/access/yb_access/yb_scan.c
+++ src/postgres/src/backend/access/yb_access/yb_scan.c
@@ -2202,9 +2202,6 @@ ybcBeginScan(Relation relation,
 	tsdesc->rs_key = keys;
 	tsdesc->rs_nkeys = nkeys;
 
-	/* Reorder the keys to regular key first then yb_hash_code. */
-	ybScan->nkeys = 0;
-	ybScan->nhash_keys = 0;
 	for (int i = 0; i < nkeys; ++i)
 	{
 		ScanKey key = &keys[i];
@@ -2866,14 +2863,11 @@ void ybcIndexCostEstimate(struct PlannerInfo *root, IndexPath *path,
 	foreach(lc, path->indexclauses)
 	{
 		IndexClause *iclause = lfirst_node(IndexClause, lc);
+		int indexcol = iclause->indexcol;
 		ListCell *lc2;
-		List *indexcols = iclause->indexcols == NIL ?
-							  list_make1_int(iclause->indexcol) :
-							  iclause->indexcols;
 		foreach (lc2, iclause->indexquals)
 		{
 			RestrictInfo *rinfo = lfirst_node(RestrictInfo, lc2);
-			int indexcol = linitial_int(indexcols);
 			AttrNumber	 attnum = isprimary ? index->rd_index->indkey.values[indexcol]
 											: (indexcol + 1);
 			Expr	   *clause = rinfo->clause;
