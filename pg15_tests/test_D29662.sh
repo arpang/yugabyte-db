6d8362503993cdf98ae2209259330a13ebbd4318
Arpan Agrawal <arpan3495@gmail.com>
2023-12-11 17:37:52 +0530

    PG15 test fix

--- pg15_tests/test_D29662.sh
+++ pg15_tests/test_D29662.sh
@@ -2,7 +2,8 @@
 source "${BASH_SOURCE[0]%/*}"/common.sh
 
 # yb_get_range_split_clause should pass.
+# With D30923, pg_select should pass.
 java_test 'TestPgRegressMisc#testPgRegressMiscSerial' false
 grep_in_java_test \
-  'failed tests: [yb_create_language, yb_create_table_like, yb_depend, yb_explicit_row_lock_planning, yb_guc, yb_query_consistent_snapshot, yb_select]' \
+  'failed tests: [yb_create_language, yb_create_table_like, yb_depend, yb_explicit_row_lock_planning, yb_guc, yb_query_consistent_snapshot]' \
   'TestPgRegressMisc#testPgRegressMiscSerial'
