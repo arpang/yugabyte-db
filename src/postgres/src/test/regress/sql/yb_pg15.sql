1a1a9a1b7f6227d312bf4384feff7083491dd33c
Arpan Agrawal <arpan3495@gmail.com>
2024-01-16 11:09:17 +0530

    test

--- src/postgres/src/test/regress/sql/yb_pg15.sql
+++ src/postgres/src/test/regress/sql/yb_pg15.sql
@@ -367,3 +367,54 @@ INSERT INTO t3 VALUES (1, 1, 1);
 UPDATE t3 SET ADD = 2;
 SELECT * from t3;
 DROP TABLE t3;
+
+-- UPDATE view with INSTEAD OF UPDATE trigger
+CREATE OR REPLACE FUNCTION view_trigger() RETURNS trigger
+LANGUAGE plpgsql AS $$
+declare
+    argstr text := '';
+begin
+    for i in 0 .. TG_nargs - 1 loop
+        if i > 0 then
+            argstr := argstr || ', ';
+        end if;
+        argstr := argstr || TG_argv[i];
+    end loop;
+
+    raise notice '% % % % (%)', TG_RELNAME, TG_WHEN, TG_OP, TG_LEVEL, argstr;
+
+    if TG_LEVEL = 'ROW' then
+        if TG_OP = 'INSERT' then
+            raise NOTICE 'NEW: %', NEW;
+            INSERT INTO main_table VALUES (NEW.a, NEW.b);
+            RETURN NEW;
+        end if;
+
+        if TG_OP = 'UPDATE' then
+            raise NOTICE 'OLD: %, NEW: %', OLD, NEW;
+            UPDATE main_table SET a = NEW.a, b = NEW.b WHERE a = OLD.a AND b = OLD.b;
+            if NOT FOUND then RETURN NULL; end if;
+            RETURN NEW;
+        end if;
+
+        if TG_OP = 'DELETE' then
+            raise NOTICE 'OLD: %', OLD;
+            DELETE FROM main_table WHERE a = OLD.a AND b = OLD.b;
+            if NOT FOUND then RETURN NULL; end if;
+            RETURN OLD;
+        end if;
+    end if;
+
+    RETURN NULL;
+end;
+$$;
+
+CREATE TABLE main_table (a int, b int);
+CREATE VIEW main_view AS SELECT a, b FROM main_table;
+CREATE TRIGGER instead_of_update_trig INSTEAD OF UPDATE ON main_view
+FOR EACH ROW EXECUTE PROCEDURE view_trigger('instead_of_upd');
+INSERT INTO main_view VALUES (20, 30);
+INSERT INTO main_view VALUES (21, 31);
+UPDATE main_view SET b = 31 WHERE a = 20;
+SELECT * FROM main_view;
+DROP TABLE main_table CASCADE;
