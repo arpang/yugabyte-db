52038ce2c3d53323cdb8ae9939b185c959961144
Arpan Agrawal <arpan3495@gmail.com>
2024-03-14 18:35:44 +0530

    test

--- pg15_tests/test_D32993.sh
+++ pg15_tests/test_D32993.sh
@@ -5,3 +5,8 @@ failing_java_test TestPgRegressParallel
 grep_in_java_test \
   "Failed tests: [yb_parallel_colocated]" \
   TestPgRegressParallel
+
+failing_java_test TestPgRegressPlanner
+grep_in_java_test \
+  "Failed tests: [yb_planner_joins, yb_planner_size_estimates]" \
+  TestPgRegressPlanner
