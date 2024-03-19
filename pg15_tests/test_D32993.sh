96916f82e8142d2415a1d2881aca0c7e255f6e45
Arpan Agrawal <arpan3495@gmail.com>
2024-03-19 17:54:53 +0530

    [pg15] fix: activate function yb_cost_index

---
+++ pg15_tests/test_D32993.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+source "${BASH_SOURCE[0]%/*}"/common.sh
+
+failing_java_test TestPgRegressParallel
+grep_in_java_test \
+  "Failed tests: [yb_parallel_colocated]" \
+  TestPgRegressParallel
+
+failing_java_test TestPgRegressPlanner
+grep_in_java_test \
+  "Failed tests: [yb_planner_joins, yb_planner_size_estimates]" \
+  TestPgRegressPlanner
