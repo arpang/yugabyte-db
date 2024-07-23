1416fbab47abd7f03cc2b8c75d43aece605bf471
Arpan Agrawal <arpan3495@gmail.com>
2024-07-24 02:33:39 +0530

    fix ybctid reset logic

--- src/postgres/src/backend/access/common/heaptuple.c
+++ src/postgres/src/backend/access/common/heaptuple.c
@@ -1115,6 +1115,7 @@ heap_form_tuple(TupleDesc tupleDescriptor,
 	tuple->t_len = len;
 	ItemPointerSetInvalid(&(tuple->t_self));
 	tuple->t_tableOid = InvalidOid;
+	HEAPTUPLE_YBCTID(tuple) = 0;
 
 	HeapTupleHeaderSetDatumLength(td, len);
 	HeapTupleHeaderSetTypeId(td, tupleDescriptor->tdtypeid);
@@ -1504,6 +1505,7 @@ heap_tuple_from_minimal_tuple(MinimalTuple mtup)
 	result->t_len = len;
 	ItemPointerSetInvalid(&(result->t_self));
 	result->t_tableOid = InvalidOid;
+	HEAPTUPLE_YBCTID(result) = 0;
 	result->t_data = (HeapTupleHeader) ((char *) result + HEAPTUPLESIZE);
 	memcpy((char *) result->t_data + MINIMAL_TUPLE_OFFSET, mtup, mtup->t_len);
 	memset(result->t_data, 0, offsetof(HeapTupleHeaderData, t_infomask2));
