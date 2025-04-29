0117a86e9b07836207245eb25cc13c6936798025
Arpan Agrawal <arpan3495@gmail.com>
2025-04-30 00:02:27 +0530

    txn tests

--- src/yb/yql/pgwrapper/pg_txn-test.cc
+++ src/yb/yql/pgwrapper/pg_txn-test.cc
@@ -684,4 +684,55 @@ TEST_F(PgTxnTest, FlushLargeTransaction) {
   ASSERT_EQ(res, kValueLen * kTxnRows + kExtraValueLen * kExtraRows);
 }
 
+TEST_F(PgTxnTest, YbIndexCheckReadCommitted) {
+  ANNOTATE_UNPROTECTED_WRITE(FLAGS_yb_enable_read_committed_isolation) = true;
+  ASSERT_OK(RestartCluster());
+  auto conn =
+      ASSERT_RESULT(SetDefaultTransactionIsolation(Connect(), IsolationLevel::READ_COMMITTED));
+  int rowcount = 100;
+  ASSERT_OK(conn.Execute("CREATE TABLE abcd(a int primary key, b int, c int, d int)"));
+  ASSERT_OK(conn.Execute("CREATE INDEX abcd_b_c_d_idx ON abcd (b ASC) INCLUDE (c, d)"));
+  ASSERT_OK(conn.ExecuteFormat(
+      "INSERT INTO abcd SELECT i, i, i, i FROM generate_series(1, $0) i", rowcount));
+  ASSERT_OK(conn.Execute("SET yb_bnl_batch_size = 3"));
+  ASSERT_OK(conn.Execute("SET yb_index_check_max_bnl_batches = 1"));
+  ASSERT_OK(conn.Execute("SET yb_fetch_row_limit = 10"));  // TODO: is this required?
+  CountDownLatch latch(2);
+  TestThreadHolder holder;
+  holder.AddThreadFunctor([this, &stop_flag = holder.stop_flag(), &latch, &rowcount] {
+    auto aux_conn = ASSERT_RESULT(Connect());
+    latch.CountDown();
+    latch.Wait();
+    ASSERT_OK(aux_conn.Fetch("SELECT pg_sleep(2)"));
+    ASSERT_OK(aux_conn.ExecuteFormat("INSERT INTO abcd VALUES ($0, $0, $0, $0)", rowcount + 1));
+  });
+  latch.CountDown();
+  latch.Wait();
+  // Note: yb_index_check() should not be used with FROM clause on the base relation. It is done
+  // here to increase the operation time.
+  auto rows = ASSERT_RESULT((
+      conn.FetchRows<string>("SELECT yb_index_check('abcd_b_c_d_idx'::regclass)::text FROM abcd")));
+  ASSERT_EQ(rows.size(), rowcount);
+}
+
+TEST_F(PgTxnTest, YbIndexCheckRepeatableRead) {
+  auto conn = ASSERT_RESULT(Connect());
+  int64_t rowcount = 10;
+  ASSERT_OK(conn.Execute("CREATE TABLE abcd(a int primary key, b int, c int, d int)"));
+  ASSERT_OK(conn.Execute("CREATE INDEX abcd_b_c_d_idx ON abcd (b ASC) INCLUDE (c, d)"));
+  ASSERT_OK(conn.ExecuteFormat(
+      "INSERT INTO abcd SELECT i, i, i, i FROM generate_series(1, $0) i", rowcount));
+  ASSERT_OK(conn.Execute("SET yb_bnl_batch_size = 3"));
+  ASSERT_OK(conn.Execute("SET yb_index_check_max_bnl_batches = 1"));
+  ASSERT_OK(conn.Execute("BEGIN TRANSACTION ISOLATION LEVEL REPEATABLE READ"));
+  ASSERT_OK(conn.Fetch("SELECT count(*) from abcd"));
+
+  auto aux_conn = ASSERT_RESULT(Connect());
+  ASSERT_OK(aux_conn.ExecuteFormat("INSERT INTO abcd VALUES ($0, $0, $0, $0)", rowcount + 1));
+
+  ASSERT_OK((conn.Fetch("SELECT yb_index_check('abcd_b_c_d_idx'::regclass)")));
+  auto readrows = ASSERT_RESULT(conn.FetchRow<int64_t>("SELECT count(*) from abcd"));
+  ASSERT_EQ(readrows, rowcount);
+}
+
 } // namespace yb::pgwrapper
