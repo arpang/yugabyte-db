d510b07f10eaf9123944f4c73cb91b2fbc26bac4
Arpan Agrawal <arpan3495@gmail.com>
2025-11-06 18:24:34 +0530

    replace channel and payload column with data, minor refactoring

--- src/postgres/src/include/catalog/pg_yb_notifications.h
+++ src/postgres/src/include/catalog/pg_yb_notifications.h
@@ -27,19 +27,15 @@
  */
 CATALOG(pg_yb_notifications,8100,YbNotificationsRelationId) BKI_SHARED_RELATION BKI_ROWTYPE_OID(8101,YbNotificationsRelation_Rowtype_Id) YB_BKI_TSERVER_HOSTED(3) BKI_SCHEMA_MACRO
 {
-	/* TODO: add field code comments */
-	uuid		node;
+	uuid		sender_node;	/* uuid of node hosting the sender backend */
 
-	int32		pid;
+	int32		sender_pid;		/* pid of the sender backend */
 
-	Oid			db;
+	Oid			dbid;			/* notification's db oid */
 
 #ifdef CATALOG_VARLEN			/* variable-length fields start here */
-	text		channel BKI_FORCE_NOT_NULL;
-
-	text		payload;
+	text		data BKI_FORCE_NOT_NULL;
 #endif
-
 } FormData_pg_yb_notifications;
 
 /* ----------------
@@ -49,6 +45,6 @@ CATALOG(pg_yb_notifications,8100,YbNotificationsRelationId) BKI_SHARED_RELATION 
  */
 typedef FormData_pg_yb_notifications *Form_pg_yb_notifications;
 
-DECLARE_UNIQUE_INDEX_PKEY(pg_yb_notifications_pkey, 8102, YbNotificationsPKeyIndexId, on pg_yb_notifications using btree(node uuid_ops HASH, pid int4_ops HASH));
+DECLARE_UNIQUE_INDEX_PKEY(pg_yb_notifications_pkey, 8102, YbNotificationsPKeyIndexId, on pg_yb_notifications using btree(sender_node uuid_ops HASH, sender_pid int4_ops HASH));
 
 #endif							/* PG_YB_NOTIFICATIONS_H */
--- src/yb/yql/pgwrapper/ysql_migrations/V92__29015__notifications_table.sql
+++ src/yb/yql/pgwrapper/ysql_migrations/V92__29015__notifications_table.sql
@@ -1,11 +1,10 @@
 BEGIN;
   CREATE TABLE IF NOT EXISTS pg_catalog.pg_yb_notifications (
-    node uuid NOT NULL,
-    pid int NOT NULL,
-    db oid NOT NULL,
-    channel text COLLATE "C" NOT NULL,
-    payload text COLLATE "C",
-    CONSTRAINT pg_yb_notifications_pkey PRIMARY KEY ((node, pid) HASH)
+    sender_node uuid NOT NULL,
+    sender_pid int NOT NULL,
+    dbid oid NOT NULL,
+    data text COLLATE "C" NOT NULL,
+    CONSTRAINT pg_yb_notifications_pkey PRIMARY KEY ((sender_node, sender_pid) HASH)
       WITH (table_oid = 8102)
   ) WITH (
     oids = false,
