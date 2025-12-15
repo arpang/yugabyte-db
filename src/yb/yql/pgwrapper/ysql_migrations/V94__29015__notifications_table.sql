facb2e8ee96a96ea52ce1cdc03c4ce0e1e4f05a0
Arpan Agrawal <arpan3495@gmail.com>
2025-12-15 17:36:41 +0530

    modify the pg_yb_notifications schema

--- src/yb/yql/pgwrapper/ysql_migrations/V94__29015__notifications_table.sql
+++ src/yb/yql/pgwrapper/ysql_migrations/V94__29015__notifications_table.sql
@@ -1,15 +1,15 @@
 BEGIN;
   CREATE TABLE IF NOT EXISTS pg_catalog.pg_yb_notifications (
-    sender_node uuid NOT NULL,
+    notif_uuid uuid NOT NULL,
+    sender_node_uuid uuid NOT NULL,
     sender_pid int NOT NULL,
-    dbid oid NOT NULL,
-    is_listen bool NOT NULL,
+    db_oid oid NOT NULL,
     data text COLLATE "C" NOT NULL,
-    CONSTRAINT pg_yb_notifications_pkey PRIMARY KEY ((sender_node, sender_pid) HASH)
+    CONSTRAINT pg_yb_notifications_pkey PRIMARY KEY ((notif_uuid))
       WITH (table_oid = 8103)
   ) WITH (
     oids = false,
     table_oid = 8101,
     row_type_oid = 8102
-  ) TABLESPACE pg_global SPLIT INTO 3 TABLETS;
+  ) TABLESPACE pg_global;
 COMMIT;
